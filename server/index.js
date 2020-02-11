0d60a88dbf7da0019c80a55da21de92d28d0da6b
Tia Gollaher <tiaag2013@gmail.com>
2020-02-11 11:24:28 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express'),
+      massive = require('massive'),
+      session = require('express-session'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+      app = express();
+
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    const port = SERVER_PORT;
+    app.listen(port, () => console.log(`Running on ${port}`));
+    app.set('db', db)
+    console.log('db connected')
+});
